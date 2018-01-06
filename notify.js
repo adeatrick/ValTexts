c4062f6f8c94d645b09be6119f25b5d444cd39f2
alexander <adeatrick1@gmail.com>
2018-01-06 04:39:21 -0500

    Core server/noitfy functionality added, html index page begun

--- notify.js
+++ notify.js
@@ -4,18 +4,27 @@ var bl = require('bl');
 var redisClient = require('redis').createClient(process.env.REDIS_URL);
 var client = new twilio(process.env.twilio_sid, process.env.twilio_auth);
 
-function notify(number, message){
-  client.messages.create({
-      body: message,
-      to: number,  // Text this number
-      from: process.env.twilio_number // From a valid Twilio number
-  }).then((message) => console.log(message.sid));
+module.exports.notify = function notify(userData){
+
+  let d = new Date();
+  let menu = getMenu(d, (menu) => {
+    constructMessage(d, menu, userData.menuOptions, (message) => {
+        client.messages.create({
+            body: message,
+            to: userData.phone,  // Text this number
+            from: process.env.twilio_number // From a valid Twilio number
+        }).then((message) => console.log(message.sid));
+    });
+  });
 }
 
-function generateId(d){
+function generateId(date){
+  //Example: dining-menu-2017-10-12-meals
+  let suffix = "meals";
+  let day = (date.getDate() < 10) ? "0" + date.getDate() : date.getDate();
+  let month = (date.getMonth()+1 < 10)  ? "0" + (date.getMonth()+1) : (date.getMonth()+1);
 
-  let day = (d.getDate() < 10) ? "0" + d.getDate() : d.getDate();
-  return "dining-menu-" + d.getFullYear() + "-" + (d.getMonth()+1) + "-" + day + "-meals";
+  return "dining-menu-" + date.getFullYear() + "-" + month + "-" + day + "-" + suffix;
 }
 
 function getMenu(d, callback){
@@ -40,16 +49,16 @@ function getMenu(d, callback){
   });
 }
 
-//Options is an object
+//menuOptions is an object
 //breakfast, lunch, dinner are objects within that object
 
-function constructMessage(d, menu, options, callback){
+function constructMessage(d, menu, menuOptions, callback){
 
   let msg = d.toDateString() + "\n\n"
-  let meals = Object.keys(options)
+  let meals = Object.keys(menuOptions)
   meals.forEach((mealName) => {
-    msg += ("-----" + mealName.toUpperCase() + "-----"+ "\n") //TODO: this should only happen if at least one of the options for that meal is true. Ie don't say "Breakfast: " if the options don't ask for any info on breakfast.
-    let meal = options[mealName] //meal now is the options object referenced by, say, "breakfast".
+    msg += ("-----" + mealName.toUpperCase() + "-----"+ "\n") //TODO: this should only happen if at least one of the menuOptions for that meal is true. Ie don't say "Breakfast: " if the menuOptions don't ask for any info on breakfast.
+    let meal = menuOptions[mealName] //meal now is the menuOptions object referenced by, say, "breakfast".
     foodStations = Object.keys(meal)
     foodStations.forEach(foodStation => {
       if(meal[foodStation] && menu[mealName][foodStation]){
@@ -61,49 +70,3 @@ function constructMessage(d, menu, options, callback){
   });
   callback(msg)
 }
-
-//This function serves to return a sample options object for testing. In the future, this will come from
-function getSampleOptions(){
-  let options = {}
-  options.breakfast = {}
-  options.breakfast["Breakfast/Grill"] = false
-  options.breakfast["Pastry Selection"] = false
-  options.breakfast["Smoothies"] = false
-
-  options.lunch = {}
-  options.lunch["Deli"] = false
-  options.lunch["Dessert"] = false
-  options.lunch["Lighter Side"] = false
-  options.lunch["Pasta"] = true
-  options.lunch["Salad Bar"] = true
-  options.lunch["Soup"] = true
-  options.lunch["Traditional"] = true
-
-  options.dinner = {}
-  options.dinner["Bread Selection"] = true
-  options.dinner["Dessert"] = true
-  options.dinner["Lighter Side"] = true
-  options.dinner["Pasta"] = false
-  options.dinner["Pizza"] = false
-  options.dinner["Soup"] = false
-  options.dinner["Traditional"] = false
-
-  return options
-}
-
-// =========================
-// CODE BELOW HERE EXECUTES
-// =========================
-
-let year = "2017"
-let month = "11" //DECEMBER, 0-indexed!!
-let day = "01"
-let d = new Date(year, month, day)
-getMenu(d, (menu) => {
-
-  //Here will be a loop through all relevant users. For now, it's just a single test.
-  options = getSampleOptions();
-  constructMessage(d, menu, options, (msg) => {
-    notify(process.env.test_number, msg)
-  });
-});
--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const redisClient = require('redis').createClient(process.env.REDIS_URL);
 const twilio = require('twilio');
 const twilioClient = new twilio(process.env.twilio_sid, process.env.twilio_auth);
 const MessagingResponse = twilio.twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+const CronJob = require('cron').CronJob;
+var Notifier = require('./notify')
+var cronJobMap = {};
 
 // ==============================================
 // ==============================================
 
 app.set('port', (process.env.PORT || 5000));
 app.disable('x-powered-by');
+app.use(bodyParser.json());
 
 //Receives incoming sms's
 app.post('/sms', function(req, res){
@@ -26,27 +31,22 @@ app.post('/sms', function(req, res){
       redisClient.get(phone, (err, reply) => {
         if(err){
           console.log(err);
-          twiml.message("Internal server error.");
         }
         else if(reply == null){
-          twiml.message("Already unsubscribed. For help and FAQ, please visit https://WhatsAtVal.com.");
+          console.log("Already unsubscribed: " + phone)
         }
         else{ //Unsubscribe
+          let userData = JSON.parse(reply);
+          userData.unsubscribe = true;
           redisClient.del(phone, (err, reply) => {
             if(err){
               console.log(err);
-              twiml.message("Internal server error.");
             }
-            else{
-              twiml.message("Unsubscribed");
-            }
-            res.writeHead(200, {'Content-Type': 'text/xml'});
-            res.end(twiml.toString());
           });
-          return; //We write the message in the callback in this else,
+          if(phone in cronJobMap){
+            cronJobMap[phone].stop();
+          }
         }
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
       });
     });
   }
@@ -58,73 +58,173 @@ app.post('/sms', function(req, res){
 });
 
 //Receives new signups
-app.post('/subscribe/:phone/:name/:options', function(req, res){//TODO: Replace this with URL query parameters?
+app.post('/subscribe/:phone', function(req, res){//TODO: Replace this with a userdata object passed as the request body.
 
   //TODO: server side verification/formatting on phone number. Should be formatted '+11234567890' with the extra '+1' at the start.
 
   redisClient.select(1, function(){
-    let code = (Math.floor(Math.random() * 100) + 99).toString();
-    console.log(code)
-    let userData = {
-      code: code,
-      phone: querystring.unescape(req.params.phone),
-      name: querystring.unescape(req.params.name),
-      options: JSON.parse(querystring.unescape(req.params.options))
+    let phone = querystring.unescape(req.params.phone);
+    let userData = req.body;
+    if(!checkUserData(userData)){ //If userData does not meet the template specifications.
+      res.status(400).send(JSON.stringify({err:"userData did not meet template specifications."}));
+      return;
     }
-    redisClient.set(userData.phone, JSON.stringify(userData));
+    userData.securityCode = (Math.floor(Math.random() * 900) + 100).toString(); //Random number between 100 and 999.
+    twilioClient.messages.create({
+        body: 'Your ValTexts security code expires in five minutes: ' + userData.securityCode,
+        to: phone,  // Text this number
+        from: process.env.twilio_number // From a valid Twilio number
+    }).then((message) => {
+      if(message.errorMessage){
+        res.status(400).send(JSON.stringify({err:"Phone wasn't formatted correctly or was invalid."}));
+        return;
+      }
+      redisClient.set(phone, JSON.stringify(userData), (err) => {
+        if(err){
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+        redisClient.expire(phone, 300);
+        res.sendStatus(200);
+      });
+    });
   });
 });
 
-app.post('/verify/:phone/:code', function(req, res){
+app.post('/verify/:phone/:securityCode', function(req, res){
 
   //TODO: server side verification/formatting on phone number. Should be formatted '+11234567890' with the extra '+1' at the start.
+  //TODO: server side verification on options passed in body. If they don't match the expected format, reject the entry!
 
-  redisClient.select(1, function(){
-    let phone = req.params.phone
-    let code = req.params.code
+  let phone = req.params.phone
+  let securityCode = req.params.securityCode
 
+  redisClient.select(1, function(err){
+    if(err){
+      res.sendStatus(500);
+      return;
+    }
     redisClient.get(phone, (err, reply) => {
       if(err){
-        console.log(err);
+        res.sendStatus(500);
+        return;
       }
       else if(reply == null){
-        console.log("Phone wasn't formatted correctly or was incorrect.")
+        res.status(404).send(JSON.stringify({err:"Phone number could not be found in verification database."}));
       }
       else{
         let userData = JSON.parse(reply);
-        if(code == userData.code){//correct code
+        if(securityCode == userData.securityCode){//correct code
           redisClient.del(phone, (err, reply) => {
-            redisClient.select(0, function(){
-              delete userData.code;
-              redisClient.set(phone, JSON.stringify(userData))
+            redisClient.select(0, function(err){
+              if(err){
+                res.sendStatus(500);
+                return;
+              }
+              delete userData.securityCode;
+              redisClient.set(phone, JSON.stringify(userData));
+
+              startCronJob(userData); //Schedule notification text.
+
               twilioClient.messages.create({
                   body: "Code verified. Welcome to ValTexts!",
                   to: userData.phone,  // Text this number
                   from: process.env.twilio_number // From a valid Twilio number
               });
-              res.send(JSON.stringify({reply:"Code verified. Welcome to ValTexts!"}));
+              res.status(200).send(JSON.stringify({reply:"Code verified. Welcome to ValTexts!"}));
             });
           });
         }
         else{//invalid code
-          res.send(JSON.stringify({err:"Incorrect code."})); //TODO: Should also delete old userdata in redis db1, or update code. Something like that.
+          res.status(400).send(JSON.stringify({err:"Incorrect code."}));
         }
       }
     });
-
-    redisClient.set(req.params.phone, JSON.stringify(userData));
   });
 });
 
 //TODO: This should take in a phone number (which the user might have butchered) and format it as '+11234567890' with the extra '+1' at the start.
 function formatPhone(phone){
 
+}
 
+//TODO: Better verification that the timeOptions are proper ie hours < 24. And maybe that there are the right number of keys in breakfast, lunch, dinner, etc.
+//This function checks whether the userData provided meets the standard template it is expect to conform to.
+function checkUserData(userData){
+
+  try{
+    if(userData && userData.phone && userData.name
+      && userData.timeOptions && userData.timeOptions.minutes && userData.timeOptions.hours && userData.timeOptions.days && userData.timeOptions.days.length && userData.timeOptions.days.length == 7
+      && userData.menuOptions && userData.menuOptions.breakfast && userData.menuOptions.lunch && userData.menuOptions.dinner){
+
+      return true;
+    }
+    else {
+      return false;
+    }
+  } catch (e){
+    console.log("verifyUserData exception successfuly caught: \n\n" + e);
+    return false;
+  }
+}
+
+//Returns a cron string for when to run a certain
+function getCronStr(userData){
+  let dayStr = "";
+  for(var i = 0; i < userData.timeOptions.days.length; i++){
+    dayStr += (i + ",")
+  }
+  dayStr = dayStr.substring(0, dayStr.length - 1);
+
+  return "00 " + userData.timeOptions.minutes + " " + userData.timeOptions.hours + " * * " + dayStr;
+}
+
+//Returns a map of phone : cronJob
+function startAllCronJobs(){
+  try{
+    redisClient.select(0, (err) => {
+      if(err){
+        console.log(err);
+        return;
+      }
+      redisClient.keys("*", (err, keys) => {
+        if(err){
+          console.log(err);
+          return;
+        }
+        keys.forEach((phone) => { //each key is a phone number
+            redisClient.get(phone, (err, reply) => {
+              if(err){
+                console.log(err);
+                return;
+              }
+              let userData = JSON.parse(reply)
+              startCronJob(userData);
+            });
+          });
+        });
+      });
+  } catch (e){
+    twilioClient.messages.create({
+        body: 'ValTexts server failed to schedule cron jobs: ' + new Date().toString(),
+        to: process.env.test_number,  // Text this number
+        from: process.env.twilio_number // From a valid Twilio number
+    });
+  }
+}
+
+function startCronJob(userData){
+  let cronStr = getCronStr(userData);
+  cronJobMap[userData.phone] = new CronJob(cronStr, () => {
+    Notifier.notify(userData);
+  }, () => {}, true, 'America/New_York'); //don't do anything when the job is stopped, but do start the job now.
 }
 
 // ========================================
 // CODE BELOW HERE EXECUTES AT SERVER START
 // ========================================
+startAllCronJobs();
 
 var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
